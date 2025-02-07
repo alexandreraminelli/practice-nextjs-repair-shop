667583c02f91c274fd9377236f59c77e1ba099ac
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-07 19:46:33 -0300

    feat(auth): proteção de rotas com um middleware

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware"
+import { NextRequest } from "next/server"
+
+/**  */
+export default withAuth(
+  async function middleware(request: NextRequest) {
+    //   console.log(request)
+  },
+  {
+    isReturnToCurrentPage: true, // depois de fazer login, redireciona o usuário pra página onde estava antes
+  }
+)
+
+/** */
+export const config = {
+  matcher: [
+    /*
+     * Corresponde a todos os caminhos de solicitação, exceto aqueles que começam com::
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - auth
+     * - favicon.ico (favicon file)
+     * - robots.txt
+     * - images
+     * - login
+     * - homepage (represented with $ after beginning /)
+     */
+    "/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images|login|$).*)",
+  ],
+}
