0418d2119ce4e5692e9d1ca6ba86085d99ca8897
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-10 12:03:08 -0300

    feat(db): função de migração

---
+++ src/db/migrate.ts
@@ -0,0 +1,24 @@
+import { db } from "./index" // conexão com o db
+import { migrate } from "drizzle-orm/neon-http/migrator"
+
+/**
+ * Função principal do arquivo de migração.
+ *
+ * Executa a migração do banco de dados com base nas migrações presentes na pasta `src/db/migrate`.
+ * Se a migra o falhar, exibe uma mensagem de erro e o código de saída 1.
+ */
+const main = async () => {
+  try {
+    // Tenta executar a migração do banco de dados
+    await migrate(db, { migrationsFolder: "src/db/migrate" })
+    // Exibe uma mensagem de sucesso se a migração for concluída
+    console.log("Migration completed")
+  } catch (error) {
+    // Tratamento de erros: exibe uma mensagem de erro e encerra o processo com código de saída 1
+    console.error("Error during migration: ", error)
+    process.exit(1)
+  }
+}
+
+// Executar a função principal do arquivo
+main()
