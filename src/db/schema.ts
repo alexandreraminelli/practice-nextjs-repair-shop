c27a655e55de6cb8d6a6e9302f1fbdf3d8286c45
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-10 11:33:07 -0300

    feat(db): tabela `customers`

---
+++ src/db/schema.ts
@@ -0,0 +1,31 @@
+import { PgTable, serial, varchar, boolean, timestamp, integer, text, pgTable } from "drizzle-orm/pg-core" // data types para banco de dados
+import { relations } from "drizzle-orm" // relações entre tabelas
+
+/** Tabela de clientes. */
+export const customers = pgTable(
+  "customers", // nome da tabela
+  // colunas (nomes e data types)
+  {
+    id: serial("id").primaryKey(), // chave primária
+    // nome:
+    firstName: varchar("first_name").notNull(),
+    lastName: varchar("last_name").notNull(),
+    // contact:
+    email: varchar("email").unique().notNull(),
+    phone: varchar("phone").unique().notNull(),
+    // address:
+    address1: varchar("address1").notNull(),
+    address2: varchar("address2"),
+    city: varchar("city").notNull(),
+    state: varchar("state", { length: 2 }).notNull(),
+    zip: varchar("zip", { length: 10 }).notNull(),
+    // info
+    notes: text("notes"), // anotações
+    active: boolean("active").notNull().default(true), // clientes antigos não são deletados, mas marcados com `active: false`
+    createAt: timestamp("create_at").notNull().defaultNow(), // data e hora de criação do cliente (padrão: agora)
+    updateAt: timestamp("update_at") // data e hora de atualização do cliente
+      .notNull()
+      .defaultNow()
+      .$onUpdate(() => new Date()), // valor atualizado automaticamente ao editar a tupla
+  }
+)
