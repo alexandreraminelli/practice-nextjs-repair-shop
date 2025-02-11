2140986561994cdc5d0cdf788325547acbff10be
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-11 09:40:04 -0300

    feat(db): função que obtêm os clientes

---
+++ src/lib/queries/getCustomers.ts
@@ -0,0 +1,18 @@
+import { db } from "@/db"
+import { customers } from "@/db/schema"
+import { eq } from "drizzle-orm"
+
+/** Retorna um cliente do db se seu id corresponder ao id informado. */
+export async function getCustomers(
+  /** ID do cliente a ser buscado. */
+  id: number
+) {
+  /** Resultado da query que busca clientes no db. */
+  const customer = await db
+    .select()
+    .from(customers) // cliente a ser buscado
+    .where(eq(customers.id, id)) // condicional: ID corresponder
+
+  // retornar o cliente encontrado
+  return customer[0]
+}
