0eca9276b45aa85ce502023418451a79a842fdf9
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-18 10:55:21 -0300

    fix(schemas): correção no acesso das propriedades

--- src/zod-schemas/customer.ts
+++ src/zod-schemas/customer.ts
@@ -4,14 +4,14 @@ import { customers } from "@/db/schema" // tabela de clientes
 /** Esquema de validação para inserção de clientes.  */
 export const insertCustomerSchema = createInsertSchema(customers, {
   /* validações */
-  firstName: (schema) => schema.firstName.min(1, "First name is required"),
-  lastName: (schema) => schema.lastName.min(1, "Last name is required"),
-  address1: (schema) => schema.address1.min(1, "Address is required"),
-  city: (schema) => schema.city.min(1, "City is required"),
-  state: (schema) => schema.state.length(2, "State must be exactly 2 characters"),
-  email: (schema) => schema.email.email("Invalid email address"),
-  zip: (schema) => schema.zip.regex(/^\d{5}(-\d{4})?$/, "Invalid Zip code."),
-  phone: (schema) => schema.phone.regex(/^\d{3}-\d{3}-\d{4}$/, "Invalid phone number."),
+  firstName: (schema) => schema.min(1, "First name is required"),
+  lastName: (schema) => schema.min(1, "Last name is required"),
+  address1: (schema) => schema.min(1, "Address is required"),
+  city: (schema) => schema.min(1, "City is required"),
+  state: (schema) => schema.length(2, "State must be exactly 2 characters"),
+  email: (schema) => schema.email("Invalid email address"),
+  zip: (schema) => schema.regex(/^\d{5}(-\d{4})?$/, "Invalid Zip code."),
+  phone: (schema) => schema.regex(/^\d{3}-\d{3}-\d{4}$/, "Invalid phone number."),
 })
 
 /** Esquema de seleção de clientes. */
