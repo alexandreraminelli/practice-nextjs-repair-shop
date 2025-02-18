85b158d95c0f3d505d15baf0b21019792222f854
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-18 11:03:50 -0300

    fix(customer-form): correção de uso de operador

--- src/app/(rs)/customers/form/CustomerForm.tsx
+++ src/app/(rs)/customers/form/CustomerForm.tsx
@@ -6,7 +6,7 @@ import { Form } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
 import { insertCustomerSchema, type insertCustomerSchemaType, type selectCustomerSchemaType } from "@/zod-schemas/customer"
 
-/**  */
+/** Props do formulário de clientes. */
 type Props = {
   customer?: selectCustomerSchemaType
 }
@@ -17,17 +17,17 @@ export default function CustomerForm(
 ) {
   /** Valores padrão do formulário. */
   const defaultValues: insertCustomerSchemaType = {
-    id: customer?.id || 0,
-    firstName: customer?.firstName || "",
-    lastName: customer?.lastName || "",
-    address1: customer?.address1 || "",
-    address2: customer?.address2 || "",
-    city: customer?.city || "",
-    state: customer?.state || "",
-    zip: customer?.zip || "",
-    phone: customer?.phone || "",
-    email: customer?.email || "",
-    notes: customer?.notes || "",
+    id: customer?.id ?? 0,
+    firstName: customer?.firstName ?? "",
+    lastName: customer?.lastName ?? "",
+    address1: customer?.address1 ?? "",
+    address2: customer?.address2 ?? "",
+    city: customer?.city ?? "",
+    state: customer?.state ?? "",
+    zip: customer?.zip ?? "",
+    phone: customer?.phone ?? "",
+    email: customer?.email ?? "",
+    notes: customer?.notes ?? "",
   }
 
   /** Formulário de clientes. */
