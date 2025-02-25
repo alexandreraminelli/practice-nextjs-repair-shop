5084fea5e1b423e0d283bf2ea58a21dc058dc082
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-02-25 19:40:23 -0300

    refactor(ticket-form): remover importação duplicada do componente BackButton

--- src/app/(rs)/tickets/form/page.tsx
+++ src/app/(rs)/tickets/form/page.tsx
@@ -1,8 +1,7 @@
+import BackButton from "@/components/BackButton"
 import { getCustomer } from "@/lib/queries/getCustomer"
 import { getTicket } from "@/lib/queries/getTicket"
-import BackButton from "@/components/BackButton"
 import TicketForm from "./TicketForm"
-import { Ticket } from "lucide-react"
 
 /** Formulário para cadastro e edição de tickets. */
 export default async function TicketFormPage(
